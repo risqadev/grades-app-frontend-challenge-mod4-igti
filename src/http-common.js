07cdc1906a4ee0b3f664a98bc59b3694418232f4
Ricardo Camacho <camacho.rc@gmail.com>
2020-09-06 22:42:47 -0300

    sem variavel de ambiente

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: process.env.APP_BACKEND_URL,
+  baseURL: 'https://grades-backend-rscamacho.herokuapp.com',
   headers: {
     'Content-type': 'application/json',
   },
