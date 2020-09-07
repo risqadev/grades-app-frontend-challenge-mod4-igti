cb949a4bce2562afafda39f9fb16dc5344f2e89a
Ricardo Camacho <camacho.rc@gmail.com>
2020-09-07 14:25:00 -0300

    baseUrl manual set, env variables don't working

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://grades-backend-rscamacho.herokuapp.com',
+  // baseURL: 'https://grades-backend-rscamacho.herokuapp.com',
+  baseURL: 'http://localhost:3003',
+  // baseURL: process.env.API_URL,
   headers: {
     'Content-type': 'application/json',
   },
